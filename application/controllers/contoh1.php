573f97155a784b8ab040dede524ec4544817f092
anggimojo <anggiatmojo85@gmail.com>
2023-10-10 06:19:56 -0700

    tugas pertemuan ke 2

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Imam Nawawi
- Saya tingga di daerah Ciputat
+ echo"Nama saya Anggi Atmojo
+ Saya tingga di daerah Tambun Utara
  olah raga yang saya sukai adalah
- Bulutangkis";
+ pencak silat";
  }
 }
