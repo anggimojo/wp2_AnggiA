985b2253960901eb13e6d750cc52505eb5f2dc2d
anggimojo <anggiatmojo85@gmail.com>
2023-11-20 18:32:47 -0800

    tugas pertemuan 3 sampai ke 5

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pustaka-booking.code3';
+$config['base_url'] = 'http://localhost/wp1_anggiatmojo';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/pustaka-booking.code3';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
